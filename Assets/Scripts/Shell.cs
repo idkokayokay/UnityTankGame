ce3c3a87e5931c14202e8420a2e1598949961ea5
idkokayokay <kcjustinelorente@gmail.com>
2024-08-09 13:38:22 +1000

    Tank Game day 1

---
+++ Assets/Scripts/Shell.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+
+    public float m_MaxLifeTime = 2f;
+    public ParticleSystem m_ExplosionParticles;
+    public float m_MaxDamage = 34f;
+    void Start()
+    {
+        Destroy(gameObject, m_MaxLifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter(Collision Other)
+    {
+
+
+        m_ExplosionParticles.transform.parent = null;
+        m_ExplosionParticles.Play();
+        Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.main.duration);
+        Destroy(gameObject);
+
+    }
+
+
+}
+
+
---
+++ Assets/Scripts/TankHealth.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankHealth : MonoBehaviour
+{
+    public float m_startingHealth = 100f;
+    public ParticleSystem m_ExplosionPrefab;
+    private ParticleSystem m_ExplosionParticles;
+
+    public float m_CurrentHealth;
+    private bool m_Dead;
+
+    private void OnEnable()
+    {
+        m_CurrentHealth = m_startingHealth;
+        m_Dead = false;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        m_CurrentHealth -= amount;
+
+        if (m_CurrentHealth <= 0 && m_Dead == false)
+        {
+            OnDeath();
+        }
+    }
+
+    void OnDeath()
+    {
+        m_Dead = true;
+
+        m_ExplosionParticles.transform.position = transform.position;
+        m_ExplosionParticles.gameObject.SetActive(true);
+        m_ExplosionParticles.Play();
+
+        gameObject.SetActive(false);
+    }
+
+    private void Awake()
+    {
+        m_ExplosionParticles = Instantiate(m_ExplosionPrefab);
+        m_ExplosionParticles.gameObject.SetActive(false);
+    }
+}
